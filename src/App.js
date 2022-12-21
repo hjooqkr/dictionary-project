1a4fc73427f93ee3a443fc909269e2563e58516f
hjooqkr <113739687+hjooqkr@users.noreply.github.com>
2022-12-21 09:47:32 -0500

    added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World</header>
+      <div className="Container">
+        <header className="App-header">Hello World</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This project is coded by Hyunjoo Park and it's{" "}
+          <a href="#" target="_blank">
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
