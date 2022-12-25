16ef354176310b2752d91db7d5fbd10b58780ae8
hjooqkr <113739687+hjooqkr@users.noreply.github.com>
2022-12-25 13:35:54 -0500

    debug-typo

--- src/Results.js
+++ src/Results.js
@@ -7,8 +7,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-
-        {props.results.phonetic.map(function (phonetic, index) {
+        {props.results.phonetics.map(function (phonetic, index) {
           return (
             <div key={index}>
               <Phonetic phonetic={phonetic} />
