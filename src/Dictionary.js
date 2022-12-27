3993d6986dceaae08cdfab68b977b1cde11abeda
hjooqkr <113739687+hjooqkr@users.noreply.github.com>
2022-12-27 18:02:00 -0500

    added Photo component-authorization, api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,33 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState("");
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState("");
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
+
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelApiKey = `563492ad6f917000010000017133f644357e44d2a7c872af27af7004`;
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    axios
+      .get(pexelApiUrl, { headers: { Authorization: `Bearer ${pexelApiKey}` } })
+      .then(handlePexelResponse);
   }
+
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -42,6 +56,7 @@ export default function Dictionary() {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
